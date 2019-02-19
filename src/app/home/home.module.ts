2bf89fcc2c0674f82b98de1b5ae35f4304c3bc30
Gabriel Raducu <raducu.gabriel@gmail.com>
2019-02-19 16:32:50 +0200

    Added login component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import * as fromComponents from './components';
+import { homeRoute } from './home.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(homeRoute),
   ],
   declarations: [...fromComponents.components],
   exports: [...fromComponents.components]
--- src/app/register/components/login/login.component.ts
+++ src/app/register/components/login/login.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { catchError, take } from 'rxjs/operators';
+import { ILoginData } from 'src/app/shared/models/interfaces';
+import { RoutesNames } from 'src/app/shared/models/routes';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
+import { RegisterService } from '../../services/register/register.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent  {
+export class LoginComponent {
+  loginForm: FormGroup = this.fb.group({
+    email: this.fb.control('', [Validators.required, Validators.email]),
+    pass: this.fb.control('', Validators.required),
+  });
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private registerService: RegisterService
+  ) {}
+
+  submit() {
+    const loginData: ILoginData = this.loginForm.value;
+    this.registerService
+      .checkLogin(loginData)
+      .pipe(
+        take(1),
+        catchError((err) => {
+          console.error(err);
+          return of(false);
+        })
+      )
+      .subscribe((valid: true) => {
+        if (!valid) {
+          alert('Login failed!');
+          return;
+        }
+        this.router.navigate([RoutesNames.home]);
+      });
+  }
 }
