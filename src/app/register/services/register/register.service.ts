d07bc73922ebd915285ac20237379e8888c7d535
Gabriel Raducu <raducu.gabriel@gmail.com>
2019-02-19 16:32:15 +0200

    Added primary methods in services

--- src/app/register/services/register/register.service.ts
+++ src/app/register/services/register/register.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { ILoginData, IUserData } from 'src/app/shared/models/interfaces';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
 
 @Injectable()
 export class RegisterService {
+  constructor(private storageService: StorageService) {}
+  checkLogin(loginData: ILoginData): Observable<boolean> {
+    const user: IUserData = this.storageService.getFromStorage(loginData.mail);
+    if (!user) {
+      return throwError('No user found');
+    }
+    if (user.pass !== loginData.pass) {
+      return throwError('Passwords don\'t match');
+    }
+    this.storageService.setUserCookie(loginData.mail);
 
+    return of(true);
+  }
 }
--- src/app/shared/services/storage/storage.service.ts
+++ src/app/shared/services/storage/storage.service.ts
@@ -1,6 +1,53 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { IUserData } from '../../models/interfaces';
+import { makeid } from '../../utils/tokengenerator';
 
 @Injectable()
 export class StorageService {
+  setUserCookie(email: string) {
+    const currentCookies = document.cookie.split(';');
+    let existingCookieTime = currentCookies.find((cookieString) => cookieString.includes(`${email}_time`));
+    if (existingCookieTime) {
+      existingCookieTime = `${email}_valid=${moment().format('x')}`;
+    } else {
+      currentCookies.push(`${email}_valid=${moment().format('x')}`);
+    }
+    let existingCookieToken = currentCookies.find((cookieString) => cookieString.includes(`${email}`));
+    if (existingCookieToken) {
+      existingCookieToken = `${email}=${makeid()}`;
+    } else {
+      currentCookies.push(`${email}=${makeid()}`);
 
+    }
+    document.cookie = currentCookies.join(';');
+  }
+  isUserLoggedIn(email: string) {
+    const currentCookies = document.cookie.split(';');
+    const existingCookieTime = currentCookies.find((cookieString) => cookieString.includes(`${email}_time`));
+    const existingCookieToken = currentCookies.find((cookieString) => cookieString.includes(`${email}`));
+    let returnVal = false;
+    if (existingCookieToken && existingCookieTime) {
+      const unixTime = existingCookieTime.split('=').pop();
+      if (moment(unixTime).isAfter(moment())) {
+        returnVal = true;
+      }
+    }
+
+    return returnVal;
+  }
+
+  getFromStorage(email: string): IUserData {
+    let content: IUserData = null;
+    try {
+      content = JSON.parse(localStorage.getItem(email));
+    } catch (err) {
+      content = null;
+    }
+    return content;
+  }
+
+  setToStorage(email: string, data: IUserData) {
+    localStorage.setItem(email, JSON.stringify(data));
+  }
 }
